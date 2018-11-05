15ec6e3652a851e96a5101378fcd4d8b374ae4fa
Joshua Cherry <joshuarcherry@gmail.com>
2018-11-05 15:04:11 -0500

    added variable for php version

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,7 +14,12 @@ def test_php_is_installed(host):
     """
     Tests that php is installed
     """
-    php = host.package("php7.0-common")
+    dist_release = host.ansible(
+        "setup")["ansible_facts"]["ansible_distribution_release"]
+    if dist_release == "bionic":
+        php = host.package("php7.2-common")
+    else:
+        php = host.package("php7.0-common")
     assert php.is_installed
 
 
@@ -22,6 +27,11 @@ def test_php_running_and_enabled(host):
     """
     Tests that php is running and enabled
     """
-    php = host.service("php7.0-fpm")
+    dist_release = host.ansible(
+        "setup")["ansible_facts"]["ansible_distribution_release"]
+    if dist_release == "bionic":
+        php = host.service("php7.2-fpm")
+    else:
+        php = host.service("php7.0-fpm")
     assert php.is_running
     assert php.is_enabled
